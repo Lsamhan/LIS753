b1bf1276027a353e50655dd1e2d95ab357e2ff0c
Lsamhan <90217542+Lsamhan@users.noreply.github.com>
2021-11-08 19:24:37 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("page-title").style.fontSize = "35px";
+
+document.getElementById("message").style.fontSize = "18px";
+
+function darkMode () {
+  document.body.classList.toggle('dark-mode');
+}
+
+function message () {
+  document.body.classList.add ('dismissed');
+}
